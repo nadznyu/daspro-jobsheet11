e1c8f703e38bd21faa50a98c921d06f75044ef7b
nadznyu <nadiaalya0107@gmail.com>
2025-11-17 15:45:02 +0700

    Modifikasi No.6 Percobaan 1

--- Bioskop17.java
+++ Bioskop17.java
@@ -3,8 +3,8 @@ public class Bioskop17 {
         String[][] penonton = new String[4][2];
         
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
     }
 }
