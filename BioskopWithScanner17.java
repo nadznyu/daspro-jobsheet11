5162f3d9829c2db27c765fcfffa756263513d74c
nadznyu <nadiaalya0107@gmail.com>
2025-11-20 15:02:21 +0700

    Modifikasi No. 5 Percobaan 2

--- BioskopWithScanner17.java
+++ BioskopWithScanner17.java
@@ -24,24 +24,30 @@ public class BioskopWithScanner17 {
                 int kolom = sc.nextInt();
                 sc.nextLine();
 
-            if (penonton[baris-1][kolom-1] == null) {
-                penonton[baris-1][kolom-1] = nama;
-                System.out.println("Data penonton tersimpan!");
+                if (penonton[baris - 1][kolom - 1] == null) {
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.println("Data penonton tersimpan!");
                 } else {
-                    System.out.println("Kursi sudah ditempati oleh: "+penonton[baris-1][kolom-1]);
+                    System.out.println("Kursi sudah ditempati oleh: " + penonton[baris - 1][kolom - 1]);
                     System.out.println("Input kursi lain!");
                 }
-                } else if (menu == 2) {
+            } else if (menu == 2) {
+                System.out.println();
+                System.out.println("=== DAFTAR PENONTON ===");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            System.out.print("*** ");
+                        } else {
+                            System.out.print(penonton[i][j] + " ");
+                        }
+                    }
                     System.out.println();
-                    System.out.println("=== DAFTAR PENONTON ===");
-                    for (int i = 0; i < penonton.length; i++) {
-                    System.out.println(String.join(" ", penonton[i]));
-                } 
-                } else if (menu == 3) {
-                    System.out.println("Input data penonton selesai!");
-                    break;
-                } 
+                }
+            } else if (menu == 3) {
+                System.out.println("Input data penonton selesai!");
+                break;
             }
         }
     }
-
+}
