d6e4eb81876dcef026406f160fdd3c832e7e4e2d
nadznyu <nadiaalya0107@gmail.com>
2025-11-20 16:06:38 +0700

    Modifikasi SIAKAD

--- SIAKAD17.java
+++ SIAKAD17.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
+
 public class SIAKAD17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
 
-        for (int i = 0; i < nilai.length; i++) {
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jmlMatkul = sc.nextInt();
+
+        int[][] nilai = new int[jmlMhs][jmlMatkul];
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.println();
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
-            for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+1) + ": " );
+            for (int j = 0; j < jmlMatkul; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa / jmlMatkul);
+            System.out.println();
         }
         System.out.println("\n==================================");
-        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+        System.out.println("Rata-rata nilai setiap Mata Kuliah: ");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jmlMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jmlMhs; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul/4);
+            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / jmlMatkul);
         }
     }
 }
